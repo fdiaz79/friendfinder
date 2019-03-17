62336cf617f43f5ab6dc90ba16da77ccc6a56c56
Fabian Diaz <16745697+fdiaz79@users.noreply.github.com>
2019-03-17 00:21:40 -0400

    Full functional, no readme, no validation

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,4 +1,3 @@
-// $(document).ready(function() {
     var friends = [
         {
             "name":"Ahmed",
@@ -12,28 +11,4 @@
         }      
     ];
 
-//     $("#submit-btn").on("click", function(){
-//         var userData = {
-//             name : $("#name").val(),
-//             photo : $("#pic").val(),
-//             scores : [
-//                 $("#q1").val(),
-//                 $("#q2").val(),
-//                 $("#q3").val(),
-//                 $("#q4").val(),
-//                 $("#q5").val(),
-//                 $("#q6").val(),
-//                 $("#q7").val(),
-//                 $("#q8").val(),
-//                 $("#q9").val(),
-//                 $("#q10").val()
-//             ]
-//         }
-//         $.post("/api/friends", userData).then(function(data){
-//             $("#match-name").text(data.name);
-//             $("#match-img").attr("src", data.photo);
-//         })
-
-//     });
-     // module.exports = friends;
-// });
+    module.exports = friends;
