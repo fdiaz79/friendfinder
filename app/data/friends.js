501a5750904d0c79bb75ee81cb1986222761b216
Fabian Diaz <16745697+fdiaz79@users.noreply.github.com>
2019-03-16 23:48:55 -0400

    problems with module.exports friends

---
+++ app/data/friends.js
@@ -0,0 +1,39 @@
+// $(document).ready(function() {
+    var friends = [
+        {
+            "name":"Ahmed",
+            "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+            "scores":[5,1,4,4,5,1,2,5,4,1]
+        },
+        {
+            "name":"Mickey Mouse",
+            "photo":"https://imgc.allpostersimages.com/img/print/posters/walt-disney-mickey-mouse-classic_a-G-9067566-0.jpg",
+            "scores":[5,4,3,2,1,2,3,4,3,1]
+        }      
+    ];
+
+//     $("#submit-btn").on("click", function(){
+//         var userData = {
+//             name : $("#name").val(),
+//             photo : $("#pic").val(),
+//             scores : [
+//                 $("#q1").val(),
+//                 $("#q2").val(),
+//                 $("#q3").val(),
+//                 $("#q4").val(),
+//                 $("#q5").val(),
+//                 $("#q6").val(),
+//                 $("#q7").val(),
+//                 $("#q8").val(),
+//                 $("#q9").val(),
+//                 $("#q10").val()
+//             ]
+//         }
+//         $.post("/api/friends", userData).then(function(data){
+//             $("#match-name").text(data.name);
+//             $("#match-img").attr("src", data.photo);
+//         })
+
+//     });
+     // module.exports = friends;
+// });
